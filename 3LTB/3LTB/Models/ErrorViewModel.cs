afeb738a1336457a2ed8cea515c88cf8bae5c7b6
Alfredo Rodriguez <arodrigu12@hotmail.com>
2020-01-15 14:52:24 -0500

    3LTB ASP.NET Core 3.1

---
+++ 3LTB/3LTB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _3LTB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
