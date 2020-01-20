cff9cfac8fe87215a35acbdd7c45faeb5da1421a
Alfredo Rodriguez <arodrigu12@hotmail.com>
2020-01-20 16:10:17 -0500

    Scaffold identity into an MVC project without existing authorization

---
+++ 3LTB/3LTB/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using _3LTB.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(_3LTB.Areas.Identity.IdentityHostingStartup))]
+namespace _3LTB.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<_3LTBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("_3LTBContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<_3LTBContext>();
+            });
+        }
+    }
+}
