782480e526d7b0aac731d278ce67bb6712e9f85a
Alfredo Rodriguez <arodrigu12@hotmail.com>
2020-01-29 20:46:45 -0500

    First crack at registration form with new fields.

--- 3LTB/3LTB/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ 3LTB/3LTB/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,25 @@ namespace _3LTB.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "Employee ID")]
+            public string EmployeeID { get; set; }
+
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "Seniority Number")]
+            public string SeniorityNumber { get; set; }
+
+            [Required]
+            public string Base { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -75,7 +94,15 @@ namespace _3LTB.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser {
+                    UserName = Input.Email, 
+                    Email = Input.Email,
+                    EmployeeID = Input.EmployeeID,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    SeniorityNumber = Input.SeniorityNumber,
+                    Base = Input.Base
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
